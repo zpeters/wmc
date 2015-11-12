e99dc970835c7c3c47246803f101d505988b0d5d
Zach Peters <zpeters@gmail.com>
2015-11-11 19:09:27 -0600

    Initial add

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/tarm/goserial"
+import "log"
+
+func main() {
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n, err := s.Write([]byte("mcu.info()"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("%q", buf[:n])
+}
