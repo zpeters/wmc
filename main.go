ec42f7b0ca61a620cfa4cd8b3ddf1e21c98cfe47
Zach Peters <zpeters@gmail.com>
2015-11-11 19:53:29 -0600

    Initial comms working

--- main.go
+++ main.go
@@ -7,18 +7,33 @@ func main() {
 	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
 	s, err := serial.OpenPort(c)
 	if err != nil {
+		log.Printf("Can't open serial port\n")
 		log.Fatal(err)
 	}
 
-	n, err := s.Write([]byte("mcu.info()"))
+	_, err = s.Write([]byte("print(mcu.info())\n"))
 	if err != nil {
+		log.Printf("Can't send command\n")
 		log.Fatal(err)
 	}
 
 	buf := make([]byte, 128)
-	n, err = s.Read(buf)
+	r, err := s.Read(buf)
 	if err != nil {
+		log.Printf("Can't read from serial port\r")
 		log.Fatal(err)
 	}
-	log.Print("%q", buf[:n])
+	
+	log.Printf("Reading from Serial\n")
+	log.Printf("%#v", string(buf[:r]))
+
+	buf = make([]byte, 128)
+	r, err = s.Read(buf)
+	if err != nil {
+		log.Printf("Can't read from serial port\r")
+		log.Fatal(err)
+	}
+	
+	log.Printf("Reading from Serial\n")
+	log.Printf("%#v", string(buf[:r]))
 }
