24b71b7b8473d9b39b3b14a0404605545bf2e2ea
Zach Peters <zpeters@gmail.com>
2015-11-17 21:11:20 -0600

    Fixed put command (escaping ") Removed version command and added info to help Added "cmd" command to run arbitrary one-liners

--- wmc.go
+++ wmc.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-	"strings"
-	"log"
-	"fmt"
-	"time"
-	"os"
 	"bufio"
+	"fmt"
+	"log"
+	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 import (
-	serial "github.com/tarm/serial"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	serial "github.com/tarm/serial"
 )
 
 var Version = ""
 
 // Functions
+func cleanString(dirty string) string {
+	clean := strings.Replace(dirty, "\"", "\\\"", -1)
+	return clean
+}
 func readSerial(p serial.Port) string {
 	buf := make([]byte, 1024)
 	r, _ := p.Read(buf)
-	
+
 	return strings.TrimSpace(string(buf[:r]))
 }
 
-func writeSerial(p serial.Port, s string) (err error){
+func writeSerial(p serial.Port, s string) (err error) {
 	_, err = p.Write([]byte(s + "\n"))
 	if err != nil {
 		log.Fatal(err)
@@ -35,34 +39,46 @@ func writeSerial(p serial.Port, s string) (err error){
 }
 
 func sendCommand(p serial.Port, cmd string) (out string, err error) {
-	err = writeSerial(p, cmd)
-	_ = readSerial(p)
-	out = readSerial(p)
+	var res string
 
+	err = writeSerial(p, cmd)
+
+	// drop the first line, it's always an
+	// echo of the command being sent
+	_ = readSerial(p)
+
+	// read until we get a '>'
+	for !strings.Contains(res, ">") {
+		res = readSerial(p)
+		if !strings.Contains(res, ">") {
+			out = out + res + "\n"
+		}
+	}
 	return out, err
 }
 
 func uploadFile(p serial.Port, filename string) (err error) {
-	log.Printf("Uploading file: %s\n", filename)
+	fmt.Printf("Uploading file: %s\n", filename)
 	_, destFileName := filepath.Split(filename)
-	log.Printf("Destination name: %s\n", destFileName)
+	fmt.Printf("Destination name: %s\n", destFileName)
 
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	
-	log.Printf("Writing file...\n")
+
+	fmt.Print("Writing file\n")
 	_, _ = sendCommand(p, fmt.Sprintf("file.open('%s', 'w+')", destFileName))
-	
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		log.Print(scanner.Text())
-		_, _ = sendCommand(p, fmt.Sprintf("file.write(\"%s\")", scanner.Text()))
-		fmt.Sprintf("file.write(\"%s\")", scanner.Text())
+		cmd := fmt.Sprintf("file.writeline(\"%s\")", cleanString(scanner.Text()))
+		//fmt.Printf(cmd)
+		_, _ = sendCommand(p, cmd)
+		fmt.Printf(".")
 	}
-
+	fmt.Printf("\n")
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
@@ -77,7 +93,7 @@ func flushSerial(p serial.Port) {
 	// read until we get no response
 	for res != "" {
 		res = readSerial(p)
-	}	
+	}
 }
 
 func openSerial() *serial.Port {
@@ -89,14 +105,13 @@ func openSerial() *serial.Port {
 	}
 
 	flushSerial(*s)
-	
-	return s	
-}
 
+	return s
+}
 
 // Commands
 var cmdWmcVersion = &cobra.Command{
-	Use: "ver",
+	Use:   "ver",
 	Short: "Get the current version",
 	Run: func(cmd *cobra.Command, args []string) {
 		s := openSerial()
@@ -104,16 +119,17 @@ var cmdWmcVersion = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("Result: %s", res)
+		fmt.Printf("WifiMCU Firmware: %s", res)
 	},
 }
 
 var cmdList = &cobra.Command{
-	Use: "ls",
+	Use:   "ls",
 	Short: "List all files on device",
 	Run: func(cmd *cobra.Command, args []string) {
 		s := openSerial()
-		res, err := sendCommand(*s, "file.slist()")
+		res, err := sendCommand(*s, "for k,_ in pairs(file.list()) do print(k) end")
+		//res, err := sendCommand(*s, "file.slist()")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -123,7 +139,7 @@ var cmdList = &cobra.Command{
 }
 
 var cmdPut = &cobra.Command{
-	Use: "put [filename]",
+	Use:   "put [filename]",
 	Short: "Send a file to the device",
 	Run: func(cmd *cobra.Command, args []string) {
 		p := openSerial()
@@ -135,10 +151,10 @@ var cmdPut = &cobra.Command{
 }
 
 var cmdRm = &cobra.Command{
-	Use: "rm",
+	Use:   "rm",
 	Short: "Remove a file",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Printf("removing %s\n", args[0])
+		fmt.Printf("removing %s\n", args[0])
 		s := openSerial()
 		cmdString := fmt.Sprintf("file.remove(\"%s\")", args[0])
 		_, err := sendCommand(*s, cmdString)
@@ -149,32 +165,55 @@ var cmdRm = &cobra.Command{
 }
 
 var cmdConfig = &cobra.Command{
-	Use: "config",
+	Use:   "config",
 	Short: "Display current config",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("WMC_SERIAL: %s\n", viper.Get("serial"))
 	},
 }
 
-var cmdVersion = &cobra.Command{
-	Use: "version",
-	Short: "Display wmc version",
+var cmdRead = &cobra.Command{
+	Use:   "read",
+	Short: "read a file",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Version: %#v\n", Version)
+		s := openSerial()
+		_, _ = sendCommand(*s, "file.open(\""+args[0]+"\")")
+		_, _ = sendCommand(*s, "data=file.read()")
+		_, _ = sendCommand(*s, "file.close()")
+		res, _ := sendCommand(*s, "print(data)")
+
+		fmt.Printf("%s\n", res)
 	},
 }
 
+var cmdCommand = &cobra.Command{
+	Use:   "cmd",
+	Short: "Run an arbitrary command, needs to be in double-quotes",
+	Run: func(cmd *cobra.Command, args []string) {
+		s := openSerial()
+		res, err := sendCommand(*s, args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", res)
+	},
+}
 
 func main() {
-	fmt.Printf("Version: %s\n", Version)
-	
 	// setup our settings
 	viper.SetEnvPrefix("wmc")
 	viper.BindEnv("serial")
 	viper.SetDefault("serial", "/dev/ttyUSB0")
-	
-	var rootCmd = &cobra.Command{}
-	rootCmd.AddCommand(cmdWmcVersion, cmdList, cmdPut, cmdRm, cmdConfig, cmdVersion)
+
+	var rootCmd = &cobra.Command{
+		Use:   "wmc",
+		Short: "wmc is a cross-platform command line utility for the WifiMCU Platform",
+		Long: `wmc: ` + Version + `
+wmc is a cross-platform command line utility for the WifiMCU Platform
+GPL Licensed source code, downloads and issue tracker at
+https://github.com/zpeters/wmc`,
+	}
+	rootCmd.AddCommand(cmdWmcVersion, cmdList, cmdPut, cmdRm, cmdConfig, cmdCommand, cmdRead)
 	rootCmd.Execute()
 
 }
